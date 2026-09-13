68afcf9048bf6551d6356c280b46a5c293c82bca
NoelElva <noeleluva@gmail.com>
2026-09-13 21:12:47 +0530

    config_file

---
+++ config.js
@@ -0,0 +1,4 @@
+// Paste your NewsAPI key below. Get a free one at https://newsapi.org
+// This file travels with the extension folder, so reinstalling the
+// extension on a new machine brings the key with it automatically.
+const NEWS_API_KEY = "";
